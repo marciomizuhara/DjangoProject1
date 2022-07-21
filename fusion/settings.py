0a0e80a2319f0e8b3f35334d04e00dff5fb600f9
marciomizuhara <marciomizu@gmail.com>
2022-07-21 17:20:20 -0300

    Concluded project

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-29wym#fq+4nfc7!9^^rx4^l2rlw9(18c-963+tn=zax2t7lt=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'fusion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,10 +88,11 @@ DATABASES = {
         'PORT': '5433',
     }
 }
-"""
+'''
 DATABASES = {
     'default': dj_database_url.config()
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
